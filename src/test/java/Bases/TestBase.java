705a7f375eed015a22290d194e6eb23b4b3389dd
VitorBarbosa <vicentebarbosavitor@gmail.com>
2024-09-06 16:05:22 -0300

    Inclusão Jsons para requests Alteração POM Inclusão de TestBase para metodos uteis

---
+++ src/test/java/Bases/TestBase.java
@@ -0,0 +1,14 @@
+package Bases;
+
+import io.restassured.RestAssured.*;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import org.hamcrest.Matchers.*;
+
+
+
+
+public class TestBase {
+
+    public String BaseUrl = "https://petstore.swagger.io/";
+
+}
